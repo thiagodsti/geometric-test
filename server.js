5bd37f8606a18647adb9f346bf3d9b5ca6e90fa6
Thiago Diniz da Silveira <thiago.silveira@uppoints.com>
2018-07-17 13:34:43 -0300

    Added express

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
-});
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 8080
+
+express()
+  .use(express.static(path.join(__dirname, '/')))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
